a1813553e7974aca7b3254baba477a93a21cfe91
kuperi2026 <kupravanino201@gmail.com>
2026-08-16 08:59:02 -0400

    Create HomePage component with layout structure

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import Items from "@/components/Items";
+import HowItWorks from "@/components/HowItWorks";
+import Footer from "@/components/Footer";
+
+export default function HomePage() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Items />
+      <HowItWorks />
+      <Footer />
+    </>
+  );
+}
