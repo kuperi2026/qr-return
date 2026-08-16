cf4f5812f499053b5fe7394380261aef48895399
kuperi2026 <kupravanino201@gmail.com>
2026-08-16 09:40:52 -0400

    Create items.ts

---
+++ data/items.ts
@@ -0,0 +1,38 @@
+export const items = [
+  {
+    id: "dog",
+    icon: "🐶",
+    nameKa: "ძაღლი",
+    nameEn: "Dog",
+  },
+  {
+    id: "cat",
+    icon: "🐱",
+    nameKa: "კატა",
+    nameEn: "Cat",
+  },
+  {
+    id: "keys",
+    icon: "🔑",
+    nameKa: "გასაღები",
+    nameEn: "Keys",
+  },
+  {
+    id: "wallet",
+    icon: "👛",
+    nameKa: "საფულე",
+    nameEn: "Wallet",
+  },
+  {
+    id: "suitcase",
+    icon: "🧳",
+    nameKa: "ჩემოდანი",
+    nameEn: "Suitcase",
+  },
+  {
+    id: "bag",
+    icon: "🎒",
+    nameKa: "ჩანთა",
+    nameEn: "Bag",
+  },
+];
