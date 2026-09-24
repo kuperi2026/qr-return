a03b47f77d03a46cd49aff9dfbe911c05760bdf0
kuperi2026 <ninokuprava94@yahoo.com>
2026-09-24 09:51:41 -0400

    Hide About section from home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@
 import { useState } from "react";
 
 import HomeHeader from "./components/home/HomeHeader";
-import AboutMenu from "./components/home/AboutMenu";
 import ShopMenu from "./components/home/ShopMenu";
 import ProductInfoMenu from "./components/home/ProductInfoMenu";
 import ContactMenu from "./components/home/ContactMenu";
@@ -14,7 +13,6 @@ import PlansMenu from "./components/home/PlansMenu";
 type Lang = "ka" | "en";
 
 type Menu =
-  | "about"
   | "shop"
   | "productInfo"
   | "plans"
@@ -47,10 +45,6 @@ export default function HomePage() {
         setLanguage={setLanguage}
         toggleMenu={toggleMenu}
       />
-
-      {openMenu === "about" && (
-        <AboutMenu ka={ka} />
-      )}
 
       {openMenu === "shop" && (
         <ShopMenu ka={ka} />
