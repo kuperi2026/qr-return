4749b99159bc8be2ca345571a6ec4b22190a3516
kuperi2026 <ninokuprava94@yahoo.com>
2026-09-24 09:51:47 -0400

    Hide About section from home page

--- app/components/home/HomeHeader.tsx
+++ app/components/home/HomeHeader.tsx
@@ -15,7 +15,6 @@ import {
 type Lang = "ka" | "en";
 
 type Menu =
-  | "about"
   | "shop"
   | "productInfo"
   | "plans"
@@ -114,13 +113,6 @@ export default function HomeHeader({
           </a>
 
           <nav className="homeNav">
-
-            <button
-              onClick={() => toggleMenu("about")}
-            >
-              {ka ? "ჩვენ შესახებ" : "About"}
-              <Chevron open={openMenu === "about"} />
-            </button>
 
             <button
               onClick={() => toggleMenu("shop")}
